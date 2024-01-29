69531ce091c4635742d9350ee4a768fbb72c9ff9
AishiHuang <61915843+AishiHuang@users.noreply.github.com>
2024-01-28 20:42:16 -0600

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-python3 -m venv venv
-source venv/bin/activate
+sudo apt-get update
+sudo apt-get install stress-ng -y
+sudo apt-get install htop -y
+sudo apt-get install python3-pip -y
 pip3 install flask
-sudo yum install git -y
-# cd /home/ubuntu
+sudo apt-get install git -y
+cd /home/ubuntu
 git clone https://github.com/AishiHuang/cs498_cloud_computing.git
 cd cs498_cloud_computing
 python3 serve.py
